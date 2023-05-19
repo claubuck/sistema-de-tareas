87689018a475b57ca6ac4800dcfe3c3c438e56ec
claudio <camposclaudio@live.com>
2023-05-19 18:43:20 -0300

    creacion de tag

---
+++ database/migrations/2023_05_19_153221_create_task_tag_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Tag;
+use App\Models\Task;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task_tag', function (Blueprint $table) {
+            $table->foreignIdFor(Task::class);
+            $table->foreignIdFor(Tag::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task_tag');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -33,6 +35,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::resource('tasks',TaskController::class)->names('tasks');
+    Route::get('/task-calendar', [TaskController::class, 'calendar'])->name('task-calendar');
+    Route::get('/task-list', [TaskController::class, 'listing']);
+
+    Route::resource('tags',TagController::class)->names('tags');
 });
 
 require __DIR__.'/auth.php';
